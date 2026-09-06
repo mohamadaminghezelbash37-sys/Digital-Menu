9d16c1be385ab594f6ff61cbb57ce1ae64aa7490
mohamadaminghezelbash37-sys <mohamadaminghezelbash37@gmail.com>
2026-09-06 20:13:55 +0330

    Add files via upload

---
+++ Src/Digital Menu (Code)/gl.c
@@ -0,0 +1,345 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.14 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 8/28/2026
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega32A
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 512
+*******************************************************/
+
+#include <mega32a.h>
+#include <delay.h>
+
+// Graphic Display functions
+#include <glcd.h>
+
+// Font used for displaying text
+// on the graphic display
+#include <font5x7.h>
+#include "image.c"
+#include "image3.c"
+// Declare your global variables here
+int i,d,ls,devicenum=0;
+int outstatus[4];
+void loading(void)
+{
+             
+
+        glcd_putimagef(0,0,image,GLCD_PUTCOPY);
+        glcd_outtextxy(37,15,"Loding...");
+        delay_ms(400);
+        for(i=14;i<112;i++)
+        {
+                delay_ms(d);
+                glcd_rectangle(14,27,i,38); 
+                if(i==30) 
+                {
+                    delay_ms(100); 
+                    d=2;
+                }
+                if(i==90)
+                {
+                    delay_ms(700);
+                    d=10; 
+                }
+                    
+        } 
+        delay_ms(1500);
+        glcd_putimagef(0,0,image3,GLCD_PUTCOPY);       //MAIN PAGE
+}
+
+void enchoder(void)
+{  
+                    if(PIND.0==PIND.1) 
+                    {
+                            ls=PIND.0; 
+                    }    
+                            
+                    while(PIND.0==PIND.1);
+            
+                    if(ls==PIND.0)
+                    {
+                                    devicenum++;
+                                    if(devicenum>3)
+                                    devicenum=0; 
+                    }      
+                        
+                    if(ls==PIND.1)
+                    {
+                                    devicenum--;
+                                    if(devicenum<0)
+                                    devicenum=3;  
+                    }                       
+                    while(PIND.0!=PIND.1);                                    
+}
+
+void select(void)
+{
+                 if(devicenum==0)
+                 {                
+                                  
+                                  glcd_putimagef(0,0,image3,GLCD_PUTCOPY);
+                                  glcd_rectangle(7,5,59,25); 
+                 }  
+                 
+                 if(devicenum==1)
+                 {                
+                                  
+                                  glcd_putimagef(0,0,image3,GLCD_PUTCOPY);
+                                  glcd_rectangle(68,5,120,25);
+                 } 
+                 
+                 if(devicenum==2)
+                 {                
+                                  
+                                  glcd_putimagef(0,0,image3,GLCD_PUTCOPY);
+                                  glcd_rectangle(68,38,120,58);
+                 } 
+                 
+                 if(devicenum==3)
+                 {                
+                                  
+                                  glcd_putimagef(0,0,image3,GLCD_PUTCOPY);
+                                  glcd_rectangle(7,38,59,58);
+                 }  
+
+                    /////
+                      if(outstatus[0]==1)glcd_putpixel(10,8,1);        
+                      if(outstatus[0]==0)glcd_putpixel(10,8,0);        
+                    /////
+                      if(outstatus[1]==1)glcd_putpixel(71,8,1);
+                      if(outstatus[1]==0)glcd_putpixel(71,8,0);
+                    /////
+                      if(outstatus[2]==1)glcd_putpixel(10,41,1);
+                      if(outstatus[2]==0)glcd_putpixel(10,41,0);
+                    ///// 
+                      if(outstatus[3]==1)glcd_putpixel(71,41,1);
+                      if(outstatus[3]==0)glcd_putpixel(71,41,0);  
+                    ///// 
+                                 
+}
+
+interrupt [EXT_INT0] void ext_int0_isr(void)
+{
+              
+                      if(devicenum==0)
+                      {
+                              PORTD.3=!PORTD.3;
+                              outstatus[0]=!outstatus[0];
+                      }
+                      
+                      if(devicenum==1)
+                      {
+                              PORTD.4=!PORTD.4;
+                              outstatus[1]=!outstatus[1];
+                      }
+                      
+                      if(devicenum==2)
+                      {
+                              PORTD.6=!PORTD.6;
+                              outstatus[3]=!outstatus[3];
+                      }
+                      
+                      if(devicenum==3)
+                      {
+                              PORTD.5=!PORTD.5;
+                              outstatus[2]=!outstatus[2];
+                      }      
+                      
+
+                    /////
+                      if(outstatus[0]==1)glcd_putpixel(10,8,1);        
+                      if(outstatus[0]==0)glcd_putpixel(10,8,0);        
+                    /////
+                      if(outstatus[1]==1)glcd_putpixel(71,8,1);
+                      if(outstatus[1]==0)glcd_putpixel(71,8,0);
+                    /////
+                      if(outstatus[2]==1)glcd_putpixel(10,41,1);
+                      if(outstatus[2]==0)glcd_putpixel(10,41,0);
+                    ///// 
+                      if(outstatus[3]==1)glcd_putpixel(71,41,1);
+                      if(outstatus[3]==0)glcd_putpixel(71,41,0);  
+                    /////
+     
+}
+
+void main(void)
+{
+// Declare your local variables here
+// Variable used to store graphic display
+// controller initialization data
+GLCDINIT_t glcd_init_data;
+
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=P Bit3=P Bit2=P Bit1=P Bit0=P 
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (1<<PORTD4) | (1<<PORTD3) | (1<<PORTD2) | (1<<PORTD1) | (1<<PORTD0);
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+TCCR0=(0<<WGM00) | (0<<COM01) | (0<<COM00) | (0<<WGM01) | (0<<CS02) | (0<<CS01) | (0<<CS00);
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer1 Stopped
+// Mode: Normal top=0xFFFF
+// OC1A output: Disconnected
+// OC1B output: Disconnected
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0<<AS2;
+TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (0<<CS22) | (0<<CS21) | (0<<CS20);
+TCNT2=0x00;
+OCR2=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
+MCUCSR=(0<<ISC2);
+
+//////////////////////////////////////////////
+
+MCUCR |= (1 << ISC01);
+MCUCR &= ~(1 << ISC00);    
+GICR |= (1 << INT0);   
+#asm("sei") 
+
+//////////////////////////////////////////////    
+// USART initialization
+// USART disabled
+UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// The Analog Comparator's positive input is
+// connected to the AIN0 pin
+// The Analog Comparator's negative input is
+// connected to the AIN1 pin
+ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+SFIOR=(0<<ACME);
+
+// ADC initialization
+// ADC disabled
+ADCSRA=(0<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
+
+// SPI initialization
+// SPI disabled
+SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
+
+// TWI initialization
+// TWI disabled
+TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
+
+// Graphic Display Controller initialization
+// The KS0108 connections are specified in the
+// Project|Configure|C Compiler|Libraries|Graphic Display menu:
+// DB0 - PORTA Bit 0
+// DB1 - PORTA Bit 1
+// DB2 - PORTA Bit 2
+// DB3 - PORTA Bit 3
+// DB4 - PORTA Bit 4
+// DB5 - PORTA Bit 5
+// DB6 - PORTA Bit 6
+// DB7 - PORTA Bit 7
+// E - PORTC Bit 0
+// RD /WR - PORTC Bit 1
+// RS - PORTC Bit 2
+// /RST - PORTC Bit 3
+// CS1 - PORTC Bit 4
+// CS2 - PORTC Bit 5
+
+// Specify the current font for displaying text
+glcd_init_data.font=font5x7;
+// No function is used for reading
+// image data from external memory
+glcd_init_data.readxmem=NULL;
+// No function is used for writing
+// image data to external memory
+glcd_init_data.writexmem=NULL;
+
+glcd_init(&glcd_init_data);
+
+DDRD.3=1;
+PORTD.3=0;
+
+DDRD.4=1;
+PORTD.4=0;
+
+DDRD.5=1;
+PORTD.5=0;
+
+DDRD.6=1;
+PORTD.6=0;
+
+loading();
+
+while (1)
+      {      
+              enchoder(); 
+              select();
+      }
+}
